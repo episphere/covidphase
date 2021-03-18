bde4108f1de930c4bc73c38f1645d4758bf0e68e
Jonas Almeida <jalmeida@mathbiol.org>
2021-03-18 11:18:29 -0400

    github.io require was missing

--- covidph.js
+++ covidph.js
@@ -4,7 +4,10 @@ covidph={
     date:Date()
 }
 
-
+covidph.getData = async (url='https://data.cdc.gov/resource/muzy-jte6.json?$limit=10000')=>{
+    covidph.data = await (await fetch(url)).json()
+    return covidph.data
+}
 
 
 if(typeof(define)!='undefined'){
